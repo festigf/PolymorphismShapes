13488b868ee8342d57b06dedecbdc9759d9da156
Ruggero Cainelli <Ruggero.Cainelli@marconi.loc>
2018-04-16 16:07:55 +0200

    16.04

---
+++ PolymorphismShapes/Poligono.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PolymorphismShapes
+{
+    public class Poligono : Shape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Poligono(int x0, int y0, int Height, int Width, string id) : base(id)
+        {
+            this.X0 = x0;
+            this.Y0 = y0;
+            this.Height = Height;
+            this.Width = Width;
+        }
+
+        public Poligono() { }
+        public Poligono(string s) : base(s) { }
+        public override double Area
+        {
+            get { return Width * Height; }
+        }
+        public override double Length
+        {
+            get
+            {
+                return Width * 2 + Height * 2;
+            }
+        }
+
+        public override void Draw(PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.DrawRectangle(new Pen(Color.Blue), X0, Y0, Width, Height);
+        }
+
+
+
+
+        //public static double AreaPoligono(double[] listaPoint1,double[] listaPoint2, int n)
+        //{
+        //    double area = 0;
+        //    int j = n - 1;
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        area += (listaPoint1[j] + listaPoint1[i]) * (listaPoint2[j] - listaPoint2[i]);
+        //        j = i;
+        //    }
+        //    return Math.Abs(area / 2.0); //valore assoluto
+        //}
+    }
+}
--- PolymorphismShapes/Program.cs
+++ PolymorphismShapes/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+//polimorfismo
 class ClsForm : Form
 {
-    Button btnLength,btnArea,SaveXml,btnLoadXml,btnDraw;
-
+    Button btnLength,btnArea,SaveXml,btnLoadXml,btnDraw, btnFilling;
     Shape line = new Line(0, 0, 10, 0, "Linea");
     Shape rect = new Rectagle(10,20, 20, 30, "rect");
+    List<Shape> listaPoint1 = new List<Shape>();    
+    List<Shape> listaPoint2 = new List<Shape>();
     ListShapes listShape = new ListShapes();
     PictureBox pictureBox = new PictureBox();
     public ClsForm()
@@ -46,6 +51,14 @@ class ClsForm : Form
         };
         this.Controls.Add(btnDraw);
         this.btnDraw.Click += BtnDraw_Click;
+        btnFilling = new Button
+        {
+            Text = "DrawP",
+            Top = 125
+        };
+        this.Controls.Add(btnFilling);
+
+        this.btnFilling.Click += BtnFilling_Click;
         this.Controls.Add(pictureBox);
         pictureBox.Left = 50;
         pictureBox.Anchor = AnchorStyles.Bottom & AnchorStyles.Right;
@@ -81,12 +94,34 @@ class ClsForm : Form
         // line.Y0 = 0; line.Y1 = 0;
         this.Text = line.ToString()+"Length="+ line.Length;
     }
+
+    private void BtnFilling_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void BtnFilling_Click(object sender, PaintEventArgs e)
+    {
+        SolidBrush pennello = new SolidBrush(Color.Blue);
+        PointF punto1 = new PointF(50.0F, 50.0F);
+        PointF punto2 = new PointF(100.0F, 25.0F);
+        PointF punto3 = new PointF(200.0F, 5.0F);
+        PointF punto4 = new PointF(250.0F, 50.0F);
+        PointF punto5 = new PointF(300.0F, 100.0F);
+        PointF punto6 = new PointF(350.0F, 200.0F);
+        PointF punto7 = new PointF(250.0F, 250.0F);
+        PointF[] punti = { punto1, punto2, punto3, punto4, punto5, punto6, punto7 };
+
+        FillMode tipoRiempimento = FillMode.Winding;
+
+        e.Graphics.FillPolygon(pennello, punti, tipoRiempimento);
+    }
+
+    public void RiempimentoPoligono(Brush pennello, PointF[] punti, FillMode tiporiempimento)
+    {}
 }
 
 class Program
 { 
-
-
     static void Main(string[] args)
     {
         Application.Run(new ClsForm());
